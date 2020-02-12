68b336c26b24b0d7890928fc87e69b7eac1cc6ec
maitlandmarshallapps <maitlandmarshallapps@gmail.com>
2020-02-12 15:42:41 +1000

    Add project files.

---
+++ Coderman.Wpf.Bindings/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coderman.Wpf.Bindings
+{
+    public class ViewModel
+    {
+        public string Text { get; set; }
+    }
+}
